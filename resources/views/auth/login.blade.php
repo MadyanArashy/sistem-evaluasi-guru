690249e0193c9e8926533ecc9f32437a3e2fd583
MadyanArashy <madyan.arashy5@smk.belajar.id>
2025-08-04 13:40:48 +0700

    benerin login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,10 +53,27 @@
                 <h2 class="text-3xl text-[#0058b1] font-bold mb-6 text-center">Selamat Datang!</h2>
                 <form method="POST" action="{{ route('login') }}" class="space-y-5">
                     @csrf
-                    <input type="text" name="username" placeholder="Username" required
-                        class="w-full px-4 py-3 border-2 border-gray-200 rounded-[12px] text-[15px] focus:outline-none focus:border-[#0058b1] transition duration-300" />
-                    <input type="password" name="password" placeholder="Password" required
-                        class="w-full px-4 py-3 border-2 border-gray-200 rounded-[12px] text-[15px] focus:outline-none focus:border-[#0058b1] transition duration-300" />
+                    <!-- Email -->
+                     <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="w-full px-4 py-3 border-2 border-gray-200 rounded-[12px] text-[15px] focus:outline-none focus:border-[#0058b1] transition duration-300"
+                            type="email"
+                            name="email"
+                            :value="old('email')"
+                            required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="w-full px-4 py-3 border-2 border-gray-200 rounded-[12px] text-[15px] focus:outline-none focus:border-[#0058b1] transition duration-300"
+                                        type="password"
+                                        name="password"
+                                        required autocomplete="current-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
                     <button type="submit"
                         class="w-full bg-[#0058b1] text-white py-3 text-[16px] font-semibold rounded-[12px] hover:bg-[#004494] hover:-translate-y-0.5 transition duration-300">
                         Login
