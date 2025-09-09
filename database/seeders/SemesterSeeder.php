ea8daf7b9c6729607305069e1c25ba0f73b41fbd
MadyanArashy <madyan.arashy5@smk.belajar.id>
2025-09-09 09:01:42 +0700

    feat: semester seeder

---
+++ database/seeders/SemesterSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Semester;
+use Illuminate\Database\Seeder;
+
+class SemesterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+      Semester::insert([
+        [
+          "semester" => "1",
+          "tahun_ajaran" => "2024-2025",
+        ],
+        [
+          "semester" => "1",
+          "tahun_ajaran" => "2024-2025",
+        ],
+        [
+          "semester" => "1",
+          "tahun_ajaran" => "2025-2026",
+        ],
+        [
+          "semester" => "2",
+          "tahun_ajaran" => "2025-2026",
+        ],
+      ]);
+    }
+}
